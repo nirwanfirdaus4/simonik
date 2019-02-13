4bea75d605e21b926d67b25ff31ba2ddec6d2c0a
abdulhhabibie44 <46340801+abdulhhabibie44@users.noreply.github.com>
2019-02-14 05:14:41 +0700

    template new

--- application/views/header.php
+++ application/views/header.php
@@ -1,106 +1,78 @@
- <!-- Header -->
-    <header id="header">
-        <div class="logo">
-            <a href="index.html" class="hidden-xs">
-                Material
-                <small>admin extended dark</small>
-            </a>
-            <i class="logo__trigger zmdi zmdi-menu" data-mae-action="block-open" data-mae-target="#navigation"></i>
-        </div>
+<!DOCTYPE html>
+<html>
+<head> 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Dark Bootstrap Admin by Bootstrapious.com</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap CSS-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>">
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendor/font-awesome/css/font-awesome.min.css') ?>">
+  <!-- Custom Font Icons CSS-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/font.css') ?>">
+  <!-- Google fonts - Muli-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/https://fonts.googleapis.com/css?family=Muli:300,400,700') ?>">
+  <!-- theme stylesheet-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/style.default.css') ?>" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css') ?>">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="<?php echo base_url('assets/img/favicon.ico') ?>">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+      </head>
+      <body>
+        <header class="header">   
+          <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid d-flex align-items-center justify-content-between">
+              <div class="navbar-header">
+                <!-- Navbar Header--><a href="index.html" class="navbar-brand">
+                  <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Dark</strong><strong>Admin</strong></div>
+                  <div class="brand-text brand-sm"><strong class="text-primary">D</strong><strong>A</strong></div></a>
+                  <!-- Sidebar Toggle Btn-->
+                  <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
+                </div>
+                <div class="right-menu list-inline no-margin-bottom">    
 
-        <ul class="top-menu">
-            <li class="top-menu__trigger hidden-lg hidden-md">
-                <a href="#"><i class="zmdi zmdi-search"></i></a>
-            </li>
-
-            <li class="top-menu__apps dropdown hidden-xs hidden-sm">
-                <a data-toggle="dropdown" href="#">
-                    <i class="zmdi zmdi-apps"></i>
-                </a>
-                <ul class="dropdown-menu pull-right">
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-calendar"></i>
-                            <small>Calendar</small>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-file-text"></i>
-                            <small>Files</small>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-email"></i>
-                            <small>Mail</small>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-trending-up"></i>
-                            <small>Analytics</small>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-view-headline"></i>
-                            <small>News</small>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="zmdi zmdi-image"></i>
-                            <small>Gallery</small>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="dropdown hidden-xs">
-                <a data-toggle="dropdown" href="#"><i class="zmdi zmdi-more-vert"></i></a>
-                <ul class="dropdown-menu dropdown-menu--icon pull-right">
-                    <li class="hidden-xs">
-                        <a data-mae-action="fullscreen" href="#"><i class="zmdi zmdi-fullscreen"></i> Toggle Fullscreen</a>
-                    </li>
-                    <li>
-                        <a data-mae-action="clear-localstorage" href="#"><i class="zmdi zmdi-delete"></i> Clear Local Storage</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="zmdi zmdi-face"></i> Privacy Settings</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="zmdi zmdi-settings"></i> Other Settings</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="top-menu__alerts" data-mae-action="block-open" data-mae-target="#notifications" data-toggle="tab" data-target="#notifications__messages">
-                <a href="#"><i class="zmdi zmdi-notifications"></i></a>
-            </li>
-            <li class="top-menu__profile dropdown">
-                <a data-toggle="dropdown" href="#">
-                    <img src="demo/img/profile-pics/1.jpg" alt="">
-                </a>
-
-                <ul class="dropdown-menu pull-right dropdown-menu--icon">
-                    <li>
-                        <a href="profile-about.html"><i class="zmdi zmdi-account"></i> View Profile</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="zmdi zmdi-input-antenna"></i> Privacy Settings</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="zmdi zmdi-settings"></i> Settings</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="zmdi zmdi-time-restore"></i> Logout</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-
-        <form class="top-search">
-            <input type="text" class="top-search__input" placeholder="Search for people, files & reports">
-            <i class="zmdi zmdi-search top-search__reset"></i>
-        </form>
-    </header>
+                  <!-- Log out               -->
+                  <div class="list-inline-item logout"><a id="logout" href="login.html" class="nav-link"> <span class="d-none d-sm-inline">Logout </span><i class="icon-logout"></i></a></div>
+                </div>
+              </div>
+            </nav>
+          </header>
+          <div class="d-flex align-items-stretch">
+            <!-- Sidebar Navigation-->
+            <nav id="sidebar">
+              <!-- Sidebar Header-->
+              <div class="sidebar-header d-flex align-items-center">
+                <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                <div class="title">
+                  <h1 class="h5">Mark Stephen</h1>
+                  <p>Web Designer</p>
+                </div>
+              </div>
+              <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+              <ul class="list-unstyled">
+                <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                <li><a href="<?php echo base_url('index.php/Data_ukm/') ?>"> <i class="icon-grid"></i>Tables </a></li>
+                <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
+                <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
+                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
+                  <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                  </ul>
+                </li>
+                <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
+              </ul><span class="heading">Extras</span>
+              <ul class="list-unstyled">
+                <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
+                <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
+                <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>
+              </ul>
+            </nav>
---
+++ application/views/header_index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Dark Bootstrap Admin by Bootstrapious.com</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap CSS-->
+  <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+  <!-- Custom Font Icons CSS-->
+  <link rel="stylesheet" href="assets/css/font.css">
+  <!-- Google fonts - Muli-->
+  <link rel="stylesheet" href="assets/https://fonts.googleapis.com/css?family=Muli:300,400,700">
+  <!-- theme stylesheet-->
+  <link rel="stylesheet" href="assets/css/style.default.css" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+      </head>
+      <body>
+        <header class="header">   
+          <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid d-flex align-items-center justify-content-between">
+              <div class="navbar-header">
+                <!-- Navbar Header--><a href="index.html" class="navbar-brand">
+                  <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Dark</strong><strong>Admin</strong></div>
+                  <div class="brand-text brand-sm"><strong class="text-primary">D</strong><strong>A</strong></div></a>
+                  <!-- Sidebar Toggle Btn-->
+                  <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
+                </div>
+                <div class="right-menu list-inline no-margin-bottom">    
+
+                  <!-- Log out               -->
+                  <div class="list-inline-item logout"><a id="logout" href="login.html" class="nav-link"> <span class="d-none d-sm-inline">Logout </span><i class="icon-logout"></i></a></div>
+                </div>
+              </div>
+            </nav>
+          </header>
+          <div class="d-flex align-items-stretch">
+            <!-- Sidebar Navigation-->
+            <nav id="sidebar">
+              <!-- Sidebar Header-->
+              <div class="sidebar-header d-flex align-items-center">
+                <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+                <div class="title">
+                  <h1 class="h5">Mark Stephen</h1>
+                  <p>Web Designer</p>
+                </div>
+              </div>
+              <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+              <ul class="list-unstyled">
+                <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+                <li><a href="<?php echo base_url('index.php/Data_ukm/') ?>"> <i class="icon-grid"></i>Tables </a></li>
+                <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
+                <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
+                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
+                  <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                  </ul>
+                </li>
+                <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
+              </ul><span class="heading">Extras</span>
+              <ul class="list-unstyled">
+                <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
+                <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
+                <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>
+              </ul>
+            </nav>
