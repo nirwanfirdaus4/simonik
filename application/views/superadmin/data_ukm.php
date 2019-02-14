22d0682846362d3a82024ebbb03c6d66efb90b0b
abdulhhabibie44 <46340801+abdulhhabibie44@users.noreply.github.com>
2019-02-14 11:19:55 +0700

    nyoh

--- application/views/superadmin/data_ukm.php
+++ application/views/superadmin/data_ukm.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <?php $this->load->view('bagian/header') ?>
 <!-- Sidebar Navigation end-->
 <div class="page-content">
@@ -16,20 +19,25 @@
   </div>
   <div class="col-lg-12">
     <div class="block">
-      <div class="title"><strong>Compact Table</strong></div>
+      <div class="title"><strong>Data UKM</strong></div>
+      <button type="button" class="btn btn-primary">Tambah Data</button>
       <div class="table-responsive"> 
         <table class="table table-striped table-sm" id="myTable">
           <thead>
             <tr>
-              <!-- <th>Nomer</th> -->
+              <th>Nomer</th>
               <th>Nama UKM</th>
               <th>Action</th>
             </tr> 
           </thead>
           <tbody>
+            <?php $no=1; ?>
             <?php foreach ($array as $key) { ?>
               <tr>
+                <td><?php echo $no++ ?></td>
                 <td><?php echo $key['nama_ukm'] ?></td>
+                <td><a href="#" title="Hapus Data"><button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button></a>
+             <a href="#" title="Edit Data"><button type="button" class="btn btn-success"><i class="fa fa-edit"></i></button></a></td></td>
               </tr>
             <?php } ?>
           </tbody>
