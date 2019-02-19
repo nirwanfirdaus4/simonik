db4823aab9c91d5d1829397f028106af1fdd5c67
abdulhhabibie44 <46340801+abdulhhabibie44@users.noreply.github.com>
2019-02-19 17:26:45 +0700

    kj

--- application/views/anggota/proker.php
+++ application/views/anggota/proker.php
@@ -5,18 +5,70 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Sidebar Navigation end-->
 <div class="page-content">
   <!-- Page Header-->
-  <div class="page-header no-margin-bottom">
+  <div class="page-header">
     <div class="container-fluid">
       <h2 class="h5 no-margin-bottom">Data User</h2>
     </div>
   </div>
+  <section class="no-padding-top no-padding-bottom">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3 col-sm-6">
+          <div class="statistic-block block">
+            <div class="progress-details d-flex align-items-end justify-content-between">
+              <div class="title">
+                <div class="icon"><i class="icon-user-1"></i></div><strong>Sie Acara</strong>
+              </div>
+              <div class="number dashtext-1">27%</div>
+            </div>
+            <div class="progress progress-template">
+              <div role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-1"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="statistic-block block">
+            <div class="progress-details d-flex align-items-end justify-content-between">
+              <div class="title">
+                <div class="icon"><i class="icon-contract"></i></div><strong>Sie PDD</strong>
+              </div>
+              <div class="number dashtext-2">100%</div>
+            </div>
+            <div class="progress progress-template">
+              <div role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-2"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="statistic-block block">
+            <div class="progress-details d-flex align-items-end justify-content-between">
+              <div class="title">
+                <div class="icon"><i class="icon-paper-and-pencil"></i></div><strong>New Invoices</strong>
+              </div>
+              <div class="number dashtext-3">140</div>
+            </div>
+            <div class="progress progress-template">
+              <div role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-3"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="statistic-block block">
+            <div class="progress-details d-flex align-items-end justify-content-between">
+              <div class="title">
+                <div class="icon"><i class="icon-writing-whiteboard"></i></div><strong>All Projects</strong>
+              </div>
+              <div class="number dashtext-4">41</div>
+            </div>
+            <div class="progress progress-template">
+              <div role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100" class="progress-bar progress-bar-template dashbg-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   <!-- Breadcrumb-->
-  <div class="container-fluid">
-    <ul class="breadcrumb">
-      <li class="breadcrumb-item"><a href="<?php echo base_url('anggota/Proker/back_index') ?>">Home</a></li>
-      <li class="breadcrumb-item active">Data Proker</li>
-    </ul>
-  </div>
-  </div>
+  
 
   <?php $this->load->view('bagian/footer') ?>
