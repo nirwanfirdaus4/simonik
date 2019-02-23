5a545ec9588832d3cc7ec6670ab96aee60a7dbb3
nirwan04 <nirwanfirdaus4@gmail.com>
2019-02-24 05:22:22 +0700

    Fix color

--- application/controllers/anggota/Proker.php
+++ application/controllers/anggota/Proker.php
@@ -66,6 +66,16 @@ class Proker extends CI_Controller {
 	public function back_index()
 	{	
 		$nav_ses=0;
+		$paket['color1']='#8e44ad';
+		$paket['color2']='#2980b9';
+		$paket['color3']='#c0392b';
+		$paket['color4']='#27ae60';
+		$paket['color5']='#d35400';
+		$paket['color6']='#16a085';
+		$paket['color7']='#cc0f80';
+		$paket['color8']='#504f45';
+		$paket['color9']='#deb617';
+		$paket['color10']='#7f8c8d';
 		$this->session->set_userdata('ses_nav_proker',$nav_ses);
 		$paket['array']=$this->mdl_data_panitia->ambildata();			
 		$this->load->view('anggota/index',$paket);
