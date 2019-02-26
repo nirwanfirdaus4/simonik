b3d8f63b6dcdeb9167bc28337dc74a3b241e376f
nirwan04 <nirwanfirdaus4@gmail.com>
2019-02-27 05:42:31 +0700

    ss

--- application/controllers/admin/Data_user.php
+++ application/controllers/admin/Data_user.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Data_user extends CI_Controller {
 	public function __construct()//MEMPERSIAPKAN
-	{
+	{ 
 		parent::__construct();
 		$this->load->helper('url','form');
 		$this->load->model('mdl_data_user_ukm');
